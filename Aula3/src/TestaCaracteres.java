d9efe632e1c4bbd3b3ff4c60bb49bc0a513eec28
Daiane <adm.daianesilva@gmail.com>
2018-09-23 16:55:50 -0300

    Aula 5

---
+++ Aula3/src/TestaCaracteres.java
@@ -0,0 +1,8 @@
+public class TestaCaracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        char valor = (char) (letra + 1);
+        System.out.println(valor);
+
+    }
+}
--- Aula3/src/TestaVariaveis.java
+++ Aula3/src/TestaVariaveis.java
@@ -4,5 +4,10 @@ public class TestaVariaveis {
         System.out.println("Hello World!");
         double salario = 1350.80;
         int salarioInt = (int) salario;
+        String palavra = "alura cursos online de tecnologia";
+        System.out.println(palavra);
+
+        palavra = palavra + 2020;
+        System.out.println(palavra);
     }
 }
