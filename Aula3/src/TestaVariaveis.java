f23eb54bfc57abbf0c5d46856226ae7193be7daf
Daiane <adm.daianesilva@gmail.com>
2018-09-23 16:39:16 -0300

    Aula 2 e Aula 3

---
+++ Aula3/src/TestaVariaveis.java
@@ -0,0 +1,8 @@
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        double salario = 1350.80;
+        int salarioInt = (int) salario;
+    }
+}
